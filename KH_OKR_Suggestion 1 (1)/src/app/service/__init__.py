5057fa4dd20dcdeed1d2e069e8990f47e63d54eb
yigityavuz1 <64797918+yigityavuz1@users.noreply.github.com>
2024-03-28 17:51:01 +0300

    Delete KH_OKR_Suggestion 1 (1) directory

--- KH_OKR_Suggestion 1 (1)/src/app/service/__init__.py
+++
@@ -1,3 +0,0 @@
-from .workflow import improve_objective_workflow, improve_key_result_workflow, chatbot_workflow
-
-__all__ = ["improve_objective_workflow", "improve_key_result_workflow", "chatbot_workflow"]
